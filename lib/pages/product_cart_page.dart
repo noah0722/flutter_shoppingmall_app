c160635869c0f162604dd87a9956a6de544340cf
noah0722 <youngkwonyu@gmail.com>
2024-11-22 13:33:55 +0900

    test change
    hell gate opened

---
+++ lib/pages/product_cart_page.dart
@@ -0,0 +1,13 @@
+class ProductCartPage extends StatefulWidget {
+  const ProductCartPage({super.key});
+
+  @override
+  State<ProductCartPage> createState() => _ProductCartPageState();
+}
+
+class _ProductCartPageState extends State<ProductCartPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
